af8ddb97f9932c98cca7bddcc712a758a9c460eb
Denis <patrikeevdn@list.ru>
2020-09-21 22:50:26 +0300

    Hello

---
+++ Variant1/Lab12/Lab12/Program.cs
@@ -0,0 +1,24 @@
+/*
+ * Created by SharpDevelop.
+ * User: Saturn
+ * Date: 21.09.2020
+ * Time: 22:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Lab12
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+				
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey();
+			Hello World?!
+		}
+	}
+}
