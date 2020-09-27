80a071c547e34b478d15e25942ec4fea451b5007
Denis <patrikeevdn@list.ru>
2020-09-28 01:27:34 +0300

    finish

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,25 +1,96 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Saturn
- * Date: 22.09.2020
- * Time: 6:14
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace lab3
 {
+	class Point
+    {
+        int y, x;
+        char sym;
+        
+
+        public void SetX(int x)
+        {
+            if (x >= 0)
+                this.x = x;
+            else
+                throw new Exception("Значение x не может быть отрицательным");
+        }
+        public void SetY(int y)
+        {
+            if (y >= 0)
+                this.y = y;
+            else
+                throw new Exception("Значение y не может быть отрицательным");
+        }
+        public void SetSym(char symbol)
+        {
+            sym = symbol;
+        }
+        public void Draw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+        
+       
+}
+
+
+	
 	class Program
 	{
-		public static void Main(string[] args)
+		
+		 static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			try
+			{
+				Point p = new Point ();
+				int x,y;
+				char sym;
+				x=int.Parse(Console.ReadLine());
+				y=int.Parse(Console.ReadLine());
+				p.SetX(x);
+				p.SetY(y);
+				Console.Clear();
+				do
+				{
+				
+				p.SetX(x);
+				
+				p.SetY(y);
+				if ((x==5)&&(y!=2))
+					sym = ' ';
+						else
+							if ((x==1)&&(y==2))
+								sym = ' ';
+							else
+								if ((x<4)&&(y==2))
+									sym = '#';
+							else sym = '-';
+				
+				p.SetSym(sym);
+				p.Draw(x,y);
+				if (x<5)
+					x++;
+				else 
+				{
+					x=1;
+					y++;
+				}
+				} while ((x*y)<15);
+				Console.ReadKey();
+			}
+			catch (Exception e)
+
+
+			{
+				Console.WriteLine(e.Message);
+				Console.ReadKey();
+			}
 		}
 	}
 }
